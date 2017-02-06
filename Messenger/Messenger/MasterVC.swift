3dd2d48a79258a0257a52084ce08b59307a3909e
Dillon Baker <Dillon@root.local>
2017-02-06 12:44:23 -0500

    Implemented ability to add UITableViewCells to the UITableView

--- Messenger/Messenger/MasterVC.swift
+++ Messenger/Messenger/MasterVC.swift
@@ -8,10 +8,40 @@
 
 import UIKit
 
-class MasterVC: UIViewController {
+class MasterVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    //normally a bad idea but I am initializing this in viewDidLoad()
+    private var fakeFriendData: [String]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        fakeFriendData = ["Bob", "Joe", "Tom", "Jimmy", "Franky"]
+    }
+    
+    //MARK: - Table View functions
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "ConversationCell") as? ConversationCell {
+            
+            let friend = fakeFriendData[indexPath.row]
+            
+            cell.setFriendData(name: friend)
+            
+            return cell
+            
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fakeFriendData.count
     }
 
 }
