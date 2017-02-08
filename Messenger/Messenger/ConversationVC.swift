25d6843183d1736c250f2e4cc0299cf2148ed936
Dillon Baker <Dillon@root.local>
2017-02-08 16:47:58 -0500

    Added text field and send button in ConversationVC and animated it to move with the keyboard

--- Messenger/Messenger/ConversationVC.swift
+++ Messenger/Messenger/ConversationVC.swift
@@ -10,16 +10,48 @@ import UIKit
 
 class ConversationVC: UIViewController {
     
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var titleLabel: UILabel!
+    //needed to animate the text field and send button when the keyboard shows or hides
+    @IBOutlet weak var textViewBottomLayoutConstraint: NSLayoutConstraint!
     
-    
-    var name: String!
+    var contact: Contact!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.navigationItem.title = contact.name
         
-        nameLabel.text = name
-        titleLabel.text = name
+        setKeyboardObservers()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    func handleKeyboardNotification(notification: Notification) {
+        
+        if let info = notification.userInfo {
+            
+            //we need to know how tall the keyboard is so we can move the text view accordingly
+            let keyboardFrame: CGRect = (info[UIKeyboardFrameEndUserInfoKey] as AnyObject).cgRectValue
+            
+            let isKeyboardShowing = notification.name == Notification.Name.UIKeyboardWillShow
+            self.textViewBottomLayoutConstraint.constant = isKeyboardShowing ? keyboardFrame.height : 0
+            
+            //animate the text view position change so it moves with the keyboard
+            UIView.animate(withDuration: 0, delay: 0, options: UIViewAnimationOptions.curveEaseOut, animations: { 
+                
+                self.view.layoutIfNeeded()
+                
+            }, completion: { (completed) in })
+        }
+    }
+    
+    //MARK: - Private functions
+    private func setKeyboardObservers() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.handleKeyboardNotification(notification:)), name: Notification.Name.UIKeyboardWillShow, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.handleKeyboardNotification(notification:)), name: Notification.Name.UIKeyboardWillHide, object: nil)
+        
     }
 }
